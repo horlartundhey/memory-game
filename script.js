a4594ba7f1505b76483d3a728117af3ce5429c94
horlartundhey <horlartundhey@gmail.com>
2024-04-16 12:27:58 +0100

    some new updates for git

---
+++ script.js
@@ -0,0 +1,120 @@
+// Grab a couple of things 
+const section = document.querySelector('section');
+const playerLivesCount = document.querySelector('span');
+let playerLives = 10; 
+
+// Link 
+
+playerLivesCount.textContent = playerLives;
+
+// generate data for cards
+const getData = () => [
+    {imgSrc: './images/deku.jpg', name: "Midoriya Deku"},
+    {imgSrc: './images/gear5.png', name: "Luffy one piece"},
+    {imgSrc: './images/gojo.jpg', name: "gojo satoru"},
+    {imgSrc: './images/goku.jpg', name: "Goku"},
+    {imgSrc: './images/narut1.jpg', name: "Naruto"},
+    {imgSrc: './images/ragna.png', name: "Ragna"},
+    {imgSrc: './images/onepunch.png', name: "One Punch"},
+    {imgSrc: './images/sukuna.jpg', name: "Sukuna"},
+    {imgSrc: './images/deku.jpg', name: "Midoriya Deku"},
+    {imgSrc: './images/gear5.png', name: "Luffy one piece"},
+    {imgSrc: './images/gojo.jpg', name: "gojo satoru"},
+    {imgSrc: './images/goku.jpg', name: "Goku"},
+    {imgSrc: './images/narut1.jpg', name: "Naruto"},
+    {imgSrc: './images/ragna.png', name: "Ragna"},
+    {imgSrc: './images/onepunch.png', name: "One Punch"},
+    {imgSrc: './images/sukuna.jpg', name: "Sukuna"},
+];
+
+// create a randomizer function
+const randomize = ()=> {
+    const cardData = getData();
+    cardData.sort(() => Math.random() - 0.5);
+    return cardData;
+}
+
+// card Generator function
+const cardGenerator = () => {
+    const cardData = randomize();
+    
+    // Generate the HTML     
+    cardData.forEach((item) => {
+        const card = document.createElement('div');
+        const face = document.createElement('img');
+        const back = document.createElement('div');
+    
+        card.classList = 'card';
+        face.classList = 'face';
+        back.classList = 'back';
+        // Attach the info to the cards
+        face.src = item.imgSrc;        
+        card.setAttribute('name', item.name);
+        // Attach the cards to the section
+        section.appendChild(card);
+        card.appendChild(face);
+        card.appendChild(back);
+
+        card.addEventListener('click', (e) => {
+            card.classList.toggle('toggleCard');
+            checkCards(e);
+        })
+    });    
+};
+
+// check cards
+const checkCards = (e) =>{
+    const clickedCard = e.target;
+    clickedCard.classList.add('flipped');
+    const flippedCards = document.querySelectorAll('.flipped');
+    const toggleCard = document.querySelectorAll('toggleCard');
+    // logic    
+    if(flippedCards.length === 2){
+        if(flippedCards[0].getAttribute('name') === flippedCards[1].getAttribute('name'))
+        {
+            console.log('match');
+            flippedCards.forEach((card) => {
+                card.classList.remove('flipped');
+                card.style.pointerEvents = "none";
+            });
+        }else {
+            console.log("wrong");
+            flippedCards.forEach((card) => {
+                card.classList.remove('flipped');
+                setTimeout(()=> card.classList.remove("toggleCard"), 1000);
+            });
+            playerLives--;
+            playerLivesCount.textContent = playerLives; 
+            if(playerLives === 0){
+                restart("try again");
+            }
+        }
+    }    
+    // run a check to see if we won
+    if(toggleCard.length === 16){
+        restart("You won")
+    }
+
+}
+
+// restart
+const restart = (text) => {
+    let cardData = randomize();
+    let faces = document.querySelectorAll(".face");
+    let cards = document.querySelectorAll(".card");
+    section.style.pointerEvents = "none";
+    cardData.forEach((item, index) => {
+        cards[index].classList.remove("toggleCard");
+        // randomize
+        setTimeout(()=> {
+            cards[index].style.pointerEvents = "all";
+        faces[index].src = item.imgSrc;
+        cards[index].setAttribute('name', item.name);
+        section.style.pointerEvents = "all";
+        }, 1000);
+    });
+    playerLives = 10;
+    playerLivesCount.textContent = playerLives;
+    setTimeout(()=> window.alert(text), 1000);
+}
+cardGenerator();
